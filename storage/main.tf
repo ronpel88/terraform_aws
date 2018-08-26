52ebc70eb0cd23f559a6a1d4c960a788dc85a7f8
Ubuntu <ubuntu@ip-172-31-22-14.eu-west-2.compute.internal>
2018-08-26 08:52:45 +0000

    initial commit

---
+++ storage/main.tf
@@ -0,0 +1,17 @@
+#-----------------storage/main.tf-----------------
+
+# create a random id for s3 bucket
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+} 
+
+# create the bucket
+resource "aws_s3_bucket" "tf_code" {
+  bucket = "${var.project_name}-${random_id.tf_bucket_id.dec}"
+  acl = "private"
+  force_destroy = true
+
+  tags {
+    name = "tf_bucket"
+  }
+}
